2ae5df161568f5accb609b2b328ae48154aedbea
Prakash K <sivaprakashkoneru67@gmail.com>
2021-07-31 11:27:33 -0400

    updating emails

--- pages/api/index.js
+++ pages/api/index.js
@@ -6,15 +6,15 @@ const client = new faunadb.Client({ secret });
 
 module.exports = async (req, res) => {
   try {
-    // const saveEmail = await client.query(
-    //     q.Create(
-    //         q.Collection('emails'),
-    //         { data: {
-    //           email: req.body.email,
-    //           name: req.body.name
-    //         }},
-    //     )
-    // )
+    const saveEmail = await client.query(
+        q.Create(
+            q.Collection('emails'),
+            { data: {
+              email: req.body.email,
+              name: req.body.name
+            }},
+        )
+    )
     res.status(200).json({result: 'sucesss'});
   } catch (e) {
     res.status(500).json({ error: e.message });
